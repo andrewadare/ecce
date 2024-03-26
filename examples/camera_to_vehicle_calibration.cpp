5cfe8a66e90f1d2a2c78e89a16a5dd7798682664
Andrew Adare <andrewadare@gmail.com>
2024-03-26 14:30:52 -0700

    Move image drawing code to draw_images.cpp

--- examples/camera_to_vehicle_calibration.cpp
+++ examples/camera_to_vehicle_calibration.cpp
@@ -1,52 +1,9 @@
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/nonlinear/Marginals.h>
 
+#include <ecce/estimation.hpp>
 #include <ecce/simulation.hpp>
-#include <ecce/visualization.hpp>
 #include <random>
-
-void drawImages(const PoseMap& cameraPoses, const PoseMap& tagPoses,
-                gtsam::Cal3_S2::shared_ptr intrinsics) {
-  // Here we loop through external camera views on each side of the vehicle
-  // at each tag position in order to project the tag points to image points.
-  const std::string sides[] = {"left", "right"};
-  const std::string tagZones[] = {"front", "front_wheel", "rear_wheel"};
-
-  // Simulate onboard camera measurements of front tag points
-  const auto [ocSymbol, ocPose] = cameraPoses.at("onboard_camera");
-  Camera onboardCamera(ocPose, *intrinsics);
-
-  std::vector<gtsam::Pose3> tagsToDraw;
-
-  // Onboard camera image
-  for (const auto& side : sides) {
-    std::stringstream tagName;
-    tagName << side << "_front";
-    const auto [tagSymbol, tagPose] = tagPoses.at(tagName.str());
-    tagsToDraw.push_back(tagPose);
-  }
-  draw(tagsToDraw, onboardCamera, "onboard_camera");
-  tagsToDraw.clear();
-
-  // External camera images
-  for (const auto& side : sides) {
-    for (int i = 0; i < countViews(cameraPoses, side); ++i) {
-      std::stringstream cameraName;
-      cameraName << side << "_external_camera_" << i;
-      const auto [cameraSymbol, cameraPose] = cameraPoses.at(cameraName.str());
-      Camera camera(cameraPose, *intrinsics);
-
-      for (const auto& tagZone : tagZones) {
-        std::stringstream tagName;
-        tagName << side << "_" << tagZone;
-        const auto [tagSymbol, tagPose] = tagPoses.at(tagName.str());
-        tagsToDraw.push_back(tagPose);
-      }
-      draw(tagsToDraw, camera, cameraName.str());
-      tagsToDraw.clear();
-    }
-  }
-}
 
 // Model that the wheel markers were mounted on the vehicle with some positional
 // accuracy. This anchors them to the vehicle frame and constrains their
@@ -82,9 +39,6 @@ int main(int argc, char* argv[]) {
 
   // Calibration model for projection
   gtsam::Cal3_S2::shared_ptr intrinsics = simulateCamera();
-
-  // Draw projected tags to image files
-  drawImages(cameraPoses, tagPoses, intrinsics);
 
   // Build a factor graph:
   // - Simulate tag detections in both onboard and external cameras
