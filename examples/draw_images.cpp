d952fb1eb346f53b7d3a502dc0bb7792415d33b5
Andrew Adare <andrewadare@gmail.com>
2024-03-26 14:30:22 -0700

    Add draw_images.cpp to examples

---
+++ examples/draw_images.cpp
@@ -0,0 +1,75 @@
+
+#include <ecce/estimation.hpp>
+#include <ecce/simulation.hpp>
+#include <ecce/visualization.hpp>
+
+void drawImages(const PoseMap& cameraPoses, const PoseMap& tagPoses,
+                gtsam::Cal3_S2::shared_ptr intrinsics) {
+  // Here we loop through external camera views on each side of the vehicle
+  // at each tag position in order to project the tag points to image points.
+  const std::string sides[] = {"left", "right"};
+  const std::string tagZones[] = {"front", "front_wheel", "rear_wheel"};
+
+  // Simulate onboard camera measurements of front tag points
+  const auto [ocSymbol, ocPose] = cameraPoses.at("onboard_camera");
+  Camera onboardCamera(ocPose, *intrinsics);
+
+  std::vector<gtsam::Pose3> tagsToDraw;
+
+  auto worldPoints = localTagCorners(0.5);
+
+  // Onboard camera image
+  for (const auto& side : sides) {
+    std::stringstream tagName;
+    tagName << side << "_front";
+    const auto [tagSymbol, tagPose] = tagPoses.at(tagName.str());
+    tagsToDraw.push_back(tagPose);
+
+    std::vector<gtsam::Point2> imagePoints;
+    for (const auto& point : tagCorners(tagPose, 0.5)) {
+      imagePoints.push_back(onboardCamera.project(point));
+    }
+
+    auto mypose = pnp(worldPoints, imagePoints, intrinsics);
+    cout << mypose << endl;
+  }
+  draw(tagsToDraw, onboardCamera, "onboard_camera");
+  tagsToDraw.clear();
+
+  // External camera images
+  for (const auto& side : sides) {
+    for (int i = 0; i < countViews(cameraPoses, side); ++i) {
+      std::stringstream cameraName;
+      cameraName << side << "_external_camera_" << i;
+      const auto [cameraSymbol, cameraPose] = cameraPoses.at(cameraName.str());
+      Camera camera(cameraPose, *intrinsics);
+
+      for (const auto& tagZone : tagZones) {
+        std::stringstream tagName;
+        tagName << side << "_" << tagZone;
+        const auto [tagSymbol, tagPose] = tagPoses.at(tagName.str());
+        tagsToDraw.push_back(tagPose);
+      }
+      draw(tagsToDraw, camera, cameraName.str());
+      tagsToDraw.clear();
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 1) {
+    cout << "Usage: " << argv[0] << " (no args)" << endl;
+    return 1;
+  }
+
+  PoseMap tagPoses = simulateTagPoses();
+  PoseMap cameraPoses = lookAtTags(tagPoses);
+
+  // Calibration model for projection
+  gtsam::Cal3_S2::shared_ptr intrinsics = simulateCamera();
+
+  // Draw projected tags to image files
+  drawImages(cameraPoses, tagPoses, intrinsics);
+
+  return 0;
+}
